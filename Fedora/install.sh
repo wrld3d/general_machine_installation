b52f8c98b2f7d87ca1080f3f2b0dda793fef184f
Mark Simpson <mark.simpson@eegeo.com>
2019-06-24 15:01:54 +0100

    install.sh handles NVIDIA driver & cudnn
    This was previously handled via manually running steps in NVIDIA.md
    
    TODO: Still need to handle updating of PATH/LD_LIBRARY_PATH
    
    Buddy: Sam A

--- Fedora/install.sh
+++ Fedora/install.sh
@@ -12,6 +12,8 @@ else
   exit 1
 fi
 
+readonly install_user="$1"
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root." 
    exit 1
@@ -32,12 +34,30 @@ dnf install -y gperftools-libs
 dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+readonly cuda_rpm="cuda-repo-fedora29-10.1.105-1.x86_64.rpm"
+curl -OJ "https://developer.download.nvidia.com/compute/cuda/repos/fedora29/x86_64/$cuda_rpm"
+rpm -i "$cuda_rpm"
+dnf install cuda -y
+
+readonly cudnn_tgz="cudnn-10.1-linux-x64-v7.6.0.64.tgz"
+curl -OJ "https://rd-dependencies.s3.amazonaws.com/cuda/cudnn/$cudnn_tgz"
+tar -xzf "$cuddn_tgz"
+
+cp cuda/include/* /usr/local/cuda-10.1/include/.
+cp cuda/lib64/* /usr/local/cuda-10.1/lib64/.
+
 #Add me to dialout so that I can use screen with serial ports
-usermod -a -G dialout $1
+usermod -a -G dialout "$install_user" 
 
-su - $1 -c "echo *.pro.user$'\n'*.creator.user > .gitignore"
-su - $1 -c "git config --global core.excludesfile ~/.gitignore"
-
+su - "$install_user" -c "echo *.pro.user$'\n'*.creator.user > .gitignore"
+su - "$install_user" -c "git config --global core.excludesfile ~/.gitignore"
 
 #Graphics & office software
 dnf install -y inkscape abiword kolourpaint
+
+# TODO: export these permanently in "/home/$install_user/.bashrc"
+#export PATH=/usr/local/cuda-10.1/bin${PATH:+:${PATH}}
+#export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64\
+#	${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+echo "packages installed; please restart your machine to complete NVIDIA driver installation"
